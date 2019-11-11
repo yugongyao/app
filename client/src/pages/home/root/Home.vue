91e6e2dd5ac60f080aea80199816f4b188a94216
YuGYAO <ygy851934017@qq.com>
2019-11-11 19:18:15 +0800

    上传tab-bar切换

--- client/src/pages/home/root/Home.vue
+++ client/src/pages/home/root/Home.vue
@@ -5,11 +5,28 @@
     <router-view></router-view>
     <div class="btn1" @click="goUserDetail()">前往用户详情</div>
     <div class="btn2" @click="goDynamic()">前往动态详情</div>
+    <banner :list="bannerList"/>
+    <!-- <img src="../../../assets/banner1.jpg" alt=""> -->
   </div>
 </template>
 
 <script>
+import banner from './children/banner'
 export default {
+  components: {
+    banner
+  },
+  data(){
+    return {
+      bannerList:[
+        {id:1,picUrl:`../../../assets/banner1.jpg`},
+        {id:2,picUrl:`../../../assets/banner2.jpg`},
+        {id:3,picUrl:`../../../assets/banner3.jpeg`},
+        {id:4,picUrl:`../../../assets/banner4.jpg`},
+        {id:5,picUrl:`../../../assets/banner5.jpg`},
+      ]
+    }
+  },
   methods:{
     goUserDetail(){
       this.$router.push(`/userDetail`);
