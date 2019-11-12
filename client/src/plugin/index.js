c9f28d70747bed31b09a2563dc48a1eb3c68e816
change <2583608715@qq.com>
2019-11-12 17:45:23 +0800

    plugins

--- client/src/plugin/index.js
+++ client/src/plugin/index.js
@@ -1,7 +1,20 @@
 import AppHeader from '../components/app-header'
-
-export default{
-    install(Vue){
-        Vue.component(AppHeader.name, AppHeader)
+import AppScroll from '../components/app-scroll'
+import {
+    Icon,
+    Toast,
+    Lazyload,
+    Uploader,
+    DropdownMenu,
+    DropdownItem,
+    SwitchCell
+} from 'vant'
+export default {
+    install(Vue) {
+        // 使用vant的插件
+        Vue.use(Icon).use(Lazyload).use(Uploader).use(DropdownMenu).use(DropdownItem).use(SwitchCell);
+        Vue.prototype.$Toast = Toast;
+        Vue.component(AppHeader.name, AppHeader);
+        Vue.component(AppScroll.name, AppScroll);
     }
 }
