651c6f0090cd307568327c88b8741fae660944dd
YuGYAO <ygy851934017@qq.com>
2019-11-19 20:54:36 +0800

    加入storage.js

---
+++ client/public/js/storage.js
@@ -0,0 +1,16 @@
+
+  var storage = {
+
+    //存储
+    set(key, value) {
+      localStorage.setItem(key, JSON.stringify(value));
+    },
+    //取出数据
+    get(key) {
+      return JSON.parse(localStorage.getItem(key));
+    },
+    // 删除数据
+    remove(key) {
+      localStorage.removeItem(key);
+    }
+  }
